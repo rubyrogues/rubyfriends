cd156154933e179819f74380177667888ceb6c65
Justin Campbell <justin@justincampbell.me>
2012-10-31 19:59:18 -0400

    Remove title and subtitle from app init

--- config/initializers/rubyfriends_app_initializer.rb
+++ config/initializers/rubyfriends_app_initializer.rb
@@ -1,8 +1,3 @@
 RUBYFRIENDS_APP = RubyfriendsApp.new.tap do |app|
-
   app.default_hashtag = "RubyFriends"
-
-  app.title = 'RubyFriends.com'
-  app.subtitle = 'Community starts with friendship!'
-
 end
