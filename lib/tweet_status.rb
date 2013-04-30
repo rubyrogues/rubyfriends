b45fddec708cd0219fda78d03f5581618f6de0b6
Benjamin Fleischer <github@benjaminfleischer.com>
2013-04-30 18:36:32 -0500

    Begin testing tweet status

--- lib/tweet_status.rb
+++ lib/tweet_status.rb
@@ -6,12 +6,11 @@ class TweetStatus
 
   attr_reader :tweet
 
-  def initialize(tweet_status)
-    @tweet = tweet_status
-    @tweet_media = TweetMedia.new(tweet)
+  def initialize(tweet)
+    @tweet = tweet
   end
 
-  def_delegators :@tweet_media, :has_media?, :media_url, :media_display_url
+  def_delegators :tweet_media, :has_media?, :media_url, :media_display_url
 
   def username
     tweet.from_user.nil? ? tweet.user.screen_name : tweet.from_user
@@ -35,6 +34,12 @@ class TweetStatus
 
   def published
     true
+  end
+
+  private
+
+  def tweet_media
+    @tweet_media ||= TweetMedia.new(tweet)
   end
 
 end
