c13994b99dd7c79891c25cab6d9d3c5394f44e02
Benjamin Fleischer <github@benjaminfleischer.com>
2013-05-20 13:04:35 -0500

    Alternate heroku config
    requires setting file_storage in the application.yml
    as s3 or cloudinary and if on heroku, updating the
    heroku config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,20 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['s3_access_key_id'],
-      :aws_secret_access_key  => ENV['s3_secret_access_key'],
-      :region                 => 'us-west-1'
-    }
-    config.fog_directory  = ENV['s3_bucket']
+  if ENV['file_storage'] == 's3'
+    CarrierWave.configure do |config|
+      config.storage = :fog
+      config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['s3_access_key_id'],
+        :aws_secret_access_key  => ENV['s3_secret_access_key'],
+        :region                 => 'us-west-1'
+      }
+      config.fog_directory  = ENV['s3_bucket']
 
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
+      config.cache_dir = "#{Rails.root}/tmp/uploads"
+    end
+  end
+  if ENV['file_storage'] == 'cloudinary'
+    CarrierWave::Uploader::Base.send(:include, Cloudinary::CarrierWave)
   end
 else
   CarrierWave.configure do |config|
