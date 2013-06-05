b17d292185d0187f71a14060e2b4fc31307bbcac
Benjamin Fleischer <github@benjaminfleischer.com>
2013-06-05 08:27:13 -0500

    Move tweet display text into TweetHelper

--- app/presenters/tweet/html_presenter.rb
+++ app/presenters/tweet/html_presenter.rb
@@ -9,12 +9,11 @@ class Tweet::HtmlPresenter
     Tweet.model_name
   end
 
+  # escape unsafe html and linkify for display
   def text
     buffer = ActiveSupport::SafeBuffer.new
     buffer << tweet.tweet_text
-    link_urls(buffer)
-    link_hashtags(buffer)
-    link_usernames(buffer)
+    buffer = TweetHelper.tweet_display_text(buffer)
     buffer
   end
 
@@ -27,19 +26,6 @@ class Tweet::HtmlPresenter
   end
 
   private
-
-  def link_urls(text)
-    text.gsub! /(https?[^\s]+)/o,
-                      %q(<a href="\\1" target="_blank">\\1</a>)
-  end
-
-  def link_hashtags(text)
-    text.gsub! /#(\w+)/, '<a href="http://twitter.com/search?q=%23\\1">#\\1</a>'
-  end
-
-  def link_usernames(text)
-    text.gsub! /@(\w+)/, '<a href="http://twitter.com/\\1">@\\1</a>'
-  end
 
   def method_missing(*args)
     tweet.send *args
