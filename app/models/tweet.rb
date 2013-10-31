55fc5a312b16a71f997f4b2a72889794110b96c6
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-31 13:20:38 -0500

    Remove default_scope, retain previous behavior of published

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,13 @@
 class Tweet < ActiveRecord::Base
-  default_scope { order("published_at desc") }
 
   mount_uploader :image, ImageUploader
 
   def self.published
-    where(published: true).where("published_at is not null")
+    where(published: true).where("published_at is not null").recent_first
+  end
+
+  def self.recent_first
+    order("published_at desc")
   end
 
 end
